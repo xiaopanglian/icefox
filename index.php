7d0a6cd037d0169ba9198f6a79f35cad45674256
刘玉泉 <bushixiaopanglian@outlook.com>
2023-08-04 16:06:36 +0800

    修改结构

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Icefox theme for Typecho
  *
@@ -9,37 +10,50 @@
  */
 
 if (!defined('__TYPECHO_ROOT_DIR__')) exit;
-$this->need('header.php');
 ?>
+<!DOCTYPE HTML>
+<html>
 
-<div class="col-mb-12 col-8" id="main" role="main">
-    <?php while ($this->next()): ?>
-        <article class="post" itemscope itemtype="http://schema.org/BlogPosting">
-            <h2 class="post-title" itemprop="name headline">
-                <a itemprop="url"
-                   href="<?php $this->permalink() ?>"><?php $this->title() ?></a>
-            </h2>
-            <ul class="post-meta">
-                <li itemprop="author" itemscope itemtype="http://schema.org/Person"><?php _e('作者: '); ?><a
-                        itemprop="name" href="<?php $this->author->permalink(); ?>"
-                        rel="author"><?php $this->author(); ?></a></li>
-                <li><?php _e('时间: '); ?>
-                    <time datetime="<?php $this->date('c'); ?>" itemprop="datePublished"><?php $this->date(); ?></time>
-                </li>
-                <li><?php _e('分类: '); ?><?php $this->category(','); ?></li>
-                <li itemprop="interactionCount">
-                    <a itemprop="discussionUrl"
-                       href="<?php $this->permalink() ?>#comments"><?php $this->commentsNum('评论', '1 条评论', '%d 条评论'); ?></a>
-                </li>
-            </ul>
-            <div class="post-content" itemprop="articleBody">
-                <!-- <?php $this->content('- 阅读剩余部分 -'); ?> -->
-            </div>
-        </article>
-    <?php endwhile; ?>
+<?php $this->need('components/head.php'); ?>
 
-    <?php $this->pageNav('&laquo; 前一页', '后一页 &raquo;'); ?>
-</div><!-- end #main-->
+<body>
 
-<?php $this->need('sidebar.php'); ?>
-<?php $this->need('footer.php'); ?>
+    <!--header部分-->
+    <?php $this->need('header.php'); ?>
+
+    <!--中间内容区-->
+    <div class="container">
+        <div>
+            <div class="col-mb-12 col-8" id="main" role="main">
+                <?php while ($this->next()) : ?>
+                    <article class="post" itemscope itemtype="http://schema.org/BlogPosting">
+                        <h2 class="post-title" itemprop="name headline">
+                            <a itemprop="url" href="<?php $this->permalink() ?>"><?php $this->title() ?></a>
+                        </h2>
+                        <ul class="post-meta">
+                            <li itemprop="author" itemscope itemtype="http://schema.org/Person"><?php _e('作者: '); ?><a itemprop="name" href="<?php $this->author->permalink(); ?>" rel="author"><?php $this->author(); ?></a></li>
+                            <li><?php _e('时间: '); ?>
+                                <time datetime="<?php $this->date('c'); ?>" itemprop="datePublished"><?php $this->date(); ?></time>
+                            </li>
+                            <li><?php _e('分类: '); ?><?php $this->category(','); ?></li>
+                            <li itemprop="interactionCount">
+                                <a itemprop="discussionUrl" href="<?php $this->permalink() ?>#comments"><?php $this->commentsNum('评论', '1 条评论', '%d 条评论'); ?></a>
+                            </li>
+                        </ul>
+                        <div class="post-content" itemprop="articleBody">
+                            <!-- <?php $this->content('- 阅读剩余部分 -'); ?> -->
+                        </div>
+                    </article>
+                <?php endwhile; ?>
+
+                <?php $this->pageNav('&laquo; 前一页', '后一页 &raquo;'); ?>
+            </div><!-- end #main-->
+        </div>
+
+        <?php $this->need('sidebar.php'); ?>
+    </div>
+    <!--footer部分-->
+    <?php $this->need('footer.php'); ?>
+</body>
+
+</html>
