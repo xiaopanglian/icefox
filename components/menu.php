1910f3af2035227cc72763ac0127728978f4ca41
刘玉泉 <bushixiaopanglian@outlook.com>
2023-08-04 16:07:38 +0800

    menu

--- components/menu.php
+++ components/menu.php
@@ -1,30 +1,7 @@
-<div class="container">
-    <div class="row">
-        <div class="site-name col-mb-12 col-9">
-            <?php if ($this->options->logoUrl) : ?>
-                <a id="logo" href="<?php $this->options->siteUrl(); ?>">
-                    <img src="<?php $this->options->logoUrl() ?>" alt="<?php $this->options->title() ?>" />
-                </a>
-            <?php else : ?>
-                <a id="logo" href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title() ?></a>
-                <p class="description"><?php $this->options->description() ?></p>
-            <?php endif; ?>
-        </div>
-        <div class="site-search col-3 kit-hidden-tb">
-            <form id="search" method="post" action="<?php $this->options->siteUrl(); ?>" role="search">
-                <label for="s" class="sr-only"><?php _e('搜索关键字'); ?></label>
-                <input type="text" id="s" name="s" class="text" placeholder="<?php _e('输入关键字搜索'); ?>" />
-                <button type="submit" class="submit"><?php _e('搜索'); ?></button>
-            </form>
-        </div>
-        <div class="col-mb-12">
-            <nav id="nav-menu" class="clearfix" role="navigation">
-                <a<?php if ($this->is('index')) : ?> class="current" <?php endif; ?> href="<?php $this->options->siteUrl(); ?>"><?php _e('首页'); ?></a>
-                    <?php \Widget\Contents\Page\Rows::alloc()->to($pages); ?>
-                    <?php while ($pages->next()) : ?>
-                        <a<?php if ($this->is('page', $pages->slug)) : ?> class="current" <?php endif; ?> href="<?php $pages->permalink(); ?>" title="<?php $pages->title(); ?>"><?php $pages->title(); ?></a>
-                        <?php endwhile; ?>
-            </nav>
-        </div>
-    </div><!-- end .row -->
-</div>
+<nav id="nav-menu" class="clearfix" role="navigation">
+    <a<?php if ($this->is('index')) : ?> class="current" <?php endif; ?> href="<?php $this->options->siteUrl(); ?>"><?php _e('首页'); ?></a>
+        <?php \Widget\Contents\Page\Rows::alloc()->to($pages); ?>
+        <?php while ($pages->next()) : ?>
+            <a<?php if ($this->is('page', $pages->slug)) : ?> class="current" <?php endif; ?> href="<?php $pages->permalink(); ?>" title="<?php $pages->title(); ?>"><?php $pages->title(); ?></a>
+            <?php endwhile; ?>
+</nav>
--- header.php
+++ header.php
@@ -1,3 +1,27 @@
 <?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
+<div class="container">
+    <div class="row">
+        <div class="site-name col-mb-12 col-9">
+            <?php if ($this->options->logoUrl) : ?>
+                <a id="logo" href="<?php $this->options->siteUrl(); ?>">
+                    <img src="<?php $this->options->logoUrl() ?>" alt="<?php $this->options->title() ?>" />
+                </a>
+            <?php else : ?>
+                <a id="logo" href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title() ?></a>
+                <p class="description"><?php $this->options->description() ?></p>
+            <?php endif; ?>
+        </div>
+        <div class="site-search col-3 kit-hidden-tb">
+            <form id="search" method="post" action="<?php $this->options->siteUrl(); ?>" role="search">
+                <label for="s" class="sr-only"><?php _e('搜索关键字'); ?></label>
+                <input type="text" id="s" name="s" class="text" placeholder="<?php _e('输入关键字搜索'); ?>" />
+                <button type="submit" class="submit"><?php _e('搜索'); ?></button>
+            </form>
+        </div>
+        <div class="col-mb-12">
+            <?php $this->need('components/menu.php'); ?>
 
-<?php $this->need('components/menu.php'); ?>
+
+        </div>
+    </div><!-- end .row -->
+</div>
