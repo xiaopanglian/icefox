deccf98b1b7237ebb741dced5b12497a6686d9e8
刘玉泉 <bushixiaopanglian@outlook.com>
2023-08-04 16:51:05 +0800

    update

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
 <div class="container">
-    <div class="row">
+    <div class="flex justify-between">
         <div class="site-name col-mb-12 col-9">
             <?php if ($this->options->logoUrl) : ?>
                 <a id="logo" href="<?php $this->options->siteUrl(); ?>">
@@ -14,14 +14,15 @@
         <div class="site-search col-3 kit-hidden-tb">
             <form id="search" method="post" action="<?php $this->options->siteUrl(); ?>" role="search">
                 <label for="s" class="sr-only"><?php _e('搜索关键字'); ?></label>
-                <input type="text" id="s" name="s" class="text" placeholder="<?php _e('输入关键字搜索'); ?>" />
+                <input type="text" id="s" name="s" class="border focus:outline-none" placeholder="<?php _e('输入关键字搜索'); ?>" />
                 <button type="submit" class="submit"><?php _e('搜索'); ?></button>
             </form>
         </div>
-        <div class="col-mb-12">
-            <?php $this->need('components/menu.php'); ?>
 
-
-        </div>
     </div><!-- end .row -->
+    <div class="grid grid-cols-12">
+        <div class="">
+            <?php $this->need('components/menu.php'); ?>
+        </div>
+    </div>
 </div>
