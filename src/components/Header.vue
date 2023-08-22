160d460c996e098c370f2d03ce0966ce262e547e
刘玉泉 <bushixiaopanglian@outlook.com>
2023-08-22 16:03:55 +0800

    添加组件

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+    <header class="container pb-8">
+        <div class="">
+            <img src="http://0ru.cn/usr/uploads/2023/08/405344545.jpg" alt="top img" class="w-full object-cover"
+                style="height: calc(100vh / 3) ;" />
+        </div>
+
+        <div class="relative w-full">
+            <div class="absolute right-0 bottom-[-16px] flex flex-row h-[64px] items-center w-full justify-end pr-8">
+                <div class="text-white pr-5">
+                    朋友圈主题
+                </div>
+                <img src="http://0ru.cn/usr/uploads/2023/08/523224250.png" alt="logo"
+                    class="w-[64px] h-[64px] rounded-lg object-cover" />
+            </div>
+        </div>
+    </header>
+</template>
